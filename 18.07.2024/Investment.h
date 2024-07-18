9e011de978de8eaa162b6fe97817d28cbbf18cf6
DaniilBerdnik <daniilberdnik0@gmail.com>
2024-07-18 19:01:18 +0200

    Commit

--- 18.07.2024/Investment.h
+++ 18.07.2024/Investment.h
@@ -1,7 +1,28 @@
 #pragma once
+#include<string>
+#include<string>
+////фіо вкладника (get, set) 
+//
+//сума вкладу(get, add, del)
+//
+//розрахувати суму вкладу(кількість місяців)
+using namespace std;
 
 class Investment
 {
+	string name;
+	float money;
+public:
+	Investment();
+	Investment(string name, float money);
+
+	void setName(string name);
+	void setMoney(float money);
+
+	string getName()const;
+	float getMoney()const;
+
+	virtual float Calculate() = 0;
 
 };
 
